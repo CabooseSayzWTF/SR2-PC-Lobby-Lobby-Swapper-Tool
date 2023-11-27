344d3697335591f841943dc7b2eed1ca822a5a26
CabooseSayzWTF <itzcaboosemodz@gmail.com>
2023-11-27 15:41:11 -0600

    Add files via upload

---
+++ SR2 PC MP Lobby Swapper/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SR2_Lobby_swap
+{
+    partial class KSRXT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KSRXT));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(18, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lobby Map";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Black;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Reset Lobby",
+            "Cut SR1 Lobby",
+            "Cut Hangar Lobby",
+            "Purgatory Tut Lobby",
+            "Little Shanghai(GB)",
+            "Little Shanghai(SA)",
+            "Nuke Plant",
+            "Parthenon",
+            "Poseidon\'s Palace",
+            "The Hangar",
+            "The Pyramid",
+            "Rounds Square Mall",
+            "Shantytown",
+            "Stilwater Science",
+            "v--[Requires Custom Files]--v",
+            "MP Wardrobe",
+            "Test Level",
+            "Stilwater Penitentiary",
+            "v--[Custom Maps]--v",
+            "Anna Show",
+            "Shit 2 Play Arcade",
+            "Brotherhood HQ",
+            "Demolition Derby",
+            "Heritage Festival",
+            "Frat Party",
+            "Internet Cafe",
+            "Kanto",
+            "Run Down Loft(SR1)",
+            "Meat Plant",
+            "Prison Island",
+            "The Pyramid Extended",
+            "Red Light Methlab",
+            "The Sea Rose Pub",
+            "On Track Nightclub(SR1)",
+            "Pool Hall(SR1)",
+            "Price Mansion(SR1)",
+            "Open Legs Lodge(SR1)",
+            "On Track Nightclub(SR2)",
+            "Stilwater Caverns",
+            "Super Secret Island",
+            "Brotherhood Tanker",
+            "Tee-N-Ayy Strip Club",
+            "Tohoku Towers",
+            "Trailer Park",
+            "Ultor Ball Room",
+            "Ultor Meeting Room",
+            "Ultor Dome",
+            "Ultor Phillips Building",
+            "Ultor Yacht",
+            "Warehouse",
+            "Zombie Uprising"});
+            this.comboBox1.Location = new System.Drawing.Point(356, 121);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(457, 39);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(18, 189);
+            this.button2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(322, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Credits";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(18, 272);
+            this.button3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(322, 55);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Important Info";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.Black;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.ForeColor = System.Drawing.Color.Yellow;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Steam[MD5 = 0E8A48269EB2F91E4D2A469F45A7BC56]",
+            "OpenSpyGOG[MD5 = 4FB53345658F0DC57A25FCA53B4C6680]"});
+            this.comboBox2.Location = new System.Drawing.Point(17, 56);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(847, 39);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(280, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Choose a Game Version";
+            // 
+            // KSRXT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(882, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Fuchsia;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "KSRXT";
+            this.Text = "SR2 PC MP Lobby Swapper";
+            this.Load += new System.EventHandler(this.KSRXT_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
